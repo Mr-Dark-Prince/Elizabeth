a10e1211ecdaa855e32c28efc68138ba055cf299
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-10 16:35:21 +0530

    Update admin.py

--- ubotindo/modules/admin.py
+++ ubotindo/modules/admin.py
@@ -646,9 +646,6 @@ done easily using the bot.
  × /adminlist: list of admins in the chat
 
 *Admin only:*
- × /pin: Silently pins the message replied to - add `loud`, `notify` or `violent` to give notificaton to users.
- × /unpin: Unpins the currently pinned message.
- × /permanentpin: Set a permanent pin for supergroup chat, when an admin or telegram channel change pinned message, bot will change pinned message immediatelly
  × /invitelink: Gets private chat's invitelink.
  × /promote: Promotes the user replied to.
  × /demote: Demotes the user replied to.
