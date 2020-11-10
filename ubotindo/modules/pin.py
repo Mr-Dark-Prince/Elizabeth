4411562157c8ed83d40b08c545a17cab4b4a6b67
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-10 16:37:23 +0530

    Create pin.py

---
+++ ubotindo/modules/pin.py
@@ -0,0 +1,6 @@
+__help__ = """
+ × /pin: Silently pins the message replied to - add `loud`, `notify` or `violent` to give notificaton to users.
+ × /unpin: Unpins the currently pinned message.
+ × /permanentpin: Set a permanent pin for supergroup chat, when an admin or telegram channel change pinned message, bot will change pinned message immediatelly
+"""
+__mode_name__ = "PIN MSG"
