6e0c63683a203b247e5c012707d0119e70a1859e
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-15 15:26:02 +0530

    Update __init__.py

--- ubotindo/__init__.py
+++ ubotindo/__init__.py
@@ -204,7 +204,7 @@ else:
 # Telethon
 api_id = TELETHON_ID
 api_hash = TELETHON_HASH
-tbot = TelegramClient("ubotindo", api_id, api_hash)
+client = TelegramClient("ubotindo", api_id, api_hash)
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 
