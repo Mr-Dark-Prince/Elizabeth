c1905348d13060db9d66de18372469109dbdec36
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-15 19:47:45 +0530

    Update google.py

--- ubotindo/modules/google.py
+++ ubotindo/modules/google.py
@@ -1,67 +1,24 @@
-import glob
-import io
+import asyncio
 import os
-import re
-import urllib.request
-
-import bs4
-import html2text
 import requests
-from bing_image_downloader import downloader
-from PIL import Image
-from pymongo import MongoClient
-from telethon import *
-from telethon.tl import functions
-from telethon.tl import types
-from telethon.tl.types import *
+from bs4 import BeautifulSoup
+from datetime import datetime
+#from google_images_download import google_images_download
+import sys
+import shutil
+from re import findall
+import html2text
 
-from julia import *
-
-from julia.events import register
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["test"]
-approved_users = db.approve
-
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (await
-             tbot(functions.channels.GetParticipantRequest(chat,
-                                                           user))).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id)
-                         )).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None
-
-@register(pattern="^/google (.*)")
+@register(pattern="^/google (.*)") 
 async def _(event):
     if event.fwd_from:
         return
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
     if event.is_group:
-        if await is_register_admin(event.input_chat, event.message.sender_id):
-            pass
-        elif event.chat_id == iid and event.from_id == userss:
-            pass
-        else:
-            return
+     if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+       await event.reply("")
+       return
     # SHOW_DESCRIPTION = False
-    # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
-    input_str = event.pattern_match.group(1)
+    input_str = event.pattern_match.group(1) # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
     input_url = "https://bots.shrimadhavuk.me/search/?q={}".format(input_str)
     headers = {"USER-AGENT": "UniBorg"}
     response = requests.get(input_url, headers=headers).json()
@@ -71,43 +28,5 @@ async def _(event):
         url = result.get("url")
         description = result.get("description")
         last = html2text.html2text(description)
-        output_str += "[{}]({})\n{}\n".format(text, url, last)
-    await event.reply("{}".format(output_str),
-                      link_preview=False,
-                      parse_mode="Markdown")
-
-@register(pattern="^/img (.*)")
-async def img_sampler(event):
-    if event.fwd_from:
-        return
-    approved_userss = approved_users.find({})
-    for ch in approved_userss:
-        iid = ch["id"]
-        userss = ch["user"]
-    if event.is_group:
-        if await is_register_admin(event.input_chat, event.message.sender_id):
-            pass
-        elif event.chat_id == iid and event.from_id == userss:
-            pass
-        else:
-            return
-    query = event.pattern_match.group(1)
-    jit = f'"{query}"'
-    downloader.download(
-        jit,
-        limit=5,
-        output_dir="store",
-        adult_filter_off=False,
-        force_replace=False,
-        timeout=60,
-    )
-    os.chdir(f'./store/"{query}"')
-    types = ("*.png", "*.jpeg", "*.jpg")  # the tuple of file types
-    files_grabbed = []
-    for files in types:
-        files_grabbed.extend(glob.glob(files))
-    await event.client.send_file(event.chat_id,
-                                 files_grabbed,
-                                 reply_to=event.id)
-    os.chdir("/app/MissJuliaRobot/MissJuliaRobot")
-    os.system("rm -rf store")
+        output_str += "[{}]({})\n{}\n".format(text, url, last)       
+    await event.reply("{}".format(output_str), link_preview=False, parse_mode='Markdown')
