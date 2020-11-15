95dc77988c226fa913823917638ea95ecbcc5b32
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-16 00:49:00 +0530

    Update song.py

--- ubotindo/modules/song.py
+++ ubotindo/modules/song.py
@@ -10,7 +10,7 @@ def song(update, context):
     message = update.effective_message
     text = message.text[len('/song '):]
     dl(cmd)
-    cmd = f'{cmd}'
+    cmd = f'{song}'
     sng = db.get('title') + '.mp3'
     title = db.get('title')
     reply_text = f"Finding **{title}**"
