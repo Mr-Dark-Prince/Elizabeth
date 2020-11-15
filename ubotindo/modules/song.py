a974fd322419dff4ce1cd64c8f5ea76820826b64
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-15 16:19:54 +0530

    Create song.py

---
+++ ubotindo/modules/song.py
@@ -0,0 +1,25 @@
+from telegram import ParseMode, Update, Bot
+from telegram.ext import run_async
+from ubotindo.modules.disable import DisableAbleCommandHandler
+from ubotindo import dispatcher
+from ubotindo.modules.dll import dl, db
+import os
+
+@run_async
+def song(bot: update context):
+    message = update.effective_message
+    text = message.text[len('/song '):]
+    dl(cmd)
+    sng = db.get('title') + '.mp3'
+    title = db.get('title')
+    reply_text = f"Finding **{title}**"
+    message.reply_text(reply_text)
+    message.reply_audio(audio=sng)
+    os.remove(sng)
+    
+__help__ = """
+ - /song songname : download any song"""
+
+__mod_name__ = "Songs"
+song_handle = DisableAbleCommandHandler("song", song)
+dispatcher.add_handler(song_handle)
