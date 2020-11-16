8e8ae6846f3784fbfa3946ea1a3eb9b4ae811a7d
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-16 22:03:48 +0530

    Update __main__.py

--- ubotindo/__main__.py
+++ ubotindo/__main__.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import importlib
 import traceback
 import html
@@ -84,7 +68,7 @@ buttons = [
 
 
 buttons += [[InlineKeyboardButton(text="Help & Commands ❔",
-                                  url=f"t.me/{dispatcher.bot.username}?start=help"),
+                                  "callback_data="help_back"),
              InlineKeyboardButton(text="Support Group 🎗️",
                                   url="https://t.me/userbotindo"),
              ]]
