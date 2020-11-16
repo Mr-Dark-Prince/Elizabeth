13fc3673b0d0cc893d474220fb2619022039b53c
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-16 14:58:13 +0530

    Update imdb.py

--- ubotindo/modules/imdb.py
+++ ubotindo/modules/imdb.py
@@ -13,30 +13,9 @@ from telethon.tl import functions
 
 langi = "en"
 
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (await tbot(functions.channels.GetParticipantRequest(chat, user))).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
-        )
-    elif isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id))) \
-            .full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
-        )
-    else:
-        return None
 
 @register(pattern="^/imdb (.*)") 
 async def imdb(e):
- if e.is_group:
-  if not (await is_register_admin(e.input_chat, e.message.sender_id)):
-     return
  try:
     movie_name = e.pattern_match.group(1)
     remove_space = movie_name.split(' ')
