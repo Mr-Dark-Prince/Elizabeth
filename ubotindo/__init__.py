018b4d0bc50228194aaee5bd4a75d2ca9c7c33d7
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-16 15:34:53 +0530

    Update __init__.py

--- ubotindo/__init__.py
+++ ubotindo/__init__.py
@@ -1,24 +1,9 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import logging
 import os
 import sys
 import spamwatch
 from telethon import TelegramClient
+from redis import StrictRedis
 import telegram.ext as tg
 
 # enable logging
@@ -202,6 +187,17 @@ if SPAMWATCH is None:
     LOGGER.warning("Invalid spamwatch api")
 else:
     spamwtc = spamwatch.Client(SPAMWATCH)
+
+REDIS_URL = Config.REDIS_URI
+REDIS = StrictRedis.from_url(REDIS_URL,decode_responses=True)
+
+try:
+    REDIS.ping()
+    LOGGER.info("Your redis server is now alive!")
+    
+except BaseException:
+    raise Exception("Your redis server is not alive, please check again.")
+
 
 # Telethon
 api_id = TELETHON_ID
