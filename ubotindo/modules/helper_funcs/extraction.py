7d510fe019edd893fff8b3892a191e33d570be63
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 15:36:39 +0530

    Update extraction.py

--- ubotindo/modules/helper_funcs/extraction.py
+++ ubotindo/modules/helper_funcs/extraction.py
@@ -1,26 +1,10 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from typing import List, Optional
 
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
-from ubotindo import LOGGER
-from ubotindo.modules.users import get_user_id
+from Elizabeth import LOGGER
+from Elizabeth.modules.users import get_user_id
 
 
 def id_from_reply(message):
