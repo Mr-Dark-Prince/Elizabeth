bb543033d07f88875e93036356d60bef045b1868
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 16:41:31 +0530

    Update dbcleanup.py

--- ubotindo/modules/dbcleanup.py
+++ ubotindo/modules/dbcleanup.py
@@ -1,29 +1,13 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from time import sleep
 
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackQueryHandler, CommandHandler, run_async
 
-import ubotindo.modules.sql.global_bans_sql as gban_sql
-import ubotindo.modules.sql.users_sql as user_sql
-from ubotindo import DEV_USERS, dispatcher
-from ubotindo.modules.helper_funcs.filters import CustomFilters
+import Elizabeth.modules.sql.global_bans_sql as gban_sql
+import Elizabeth.modules.sql.users_sql as user_sql
+from Elizabeth import DEV_USERS, dispatcher
+from Elizabeth.modules.helper_funcs.filters import CustomFilters
 
 
 def get_invalid_chats(bot: Bot, update: Update, remove: bool = False):
