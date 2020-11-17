f681d2aade63a395e07f20a858ae377dda5d6504
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 15:35:35 +0530

    Update chat_status.py

--- ubotindo/modules/helper_funcs/chat_status.py
+++ ubotindo/modules/helper_funcs/chat_status.py
@@ -1,24 +1,8 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 from functools import wraps
 from telegram import User, Chat, ChatMember
 
-from ubotindo import DEL_CMDS, DEV_USERS, SUDO_USERS, WHITELIST_USERS
-from ubotindo.mwt import MWT
+from Elizabeth import DEL_CMDS, DEV_USERS, SUDO_USERS, WHITELIST_USERS
+from Elizabeth.mwt import MWT
 
 
 def can_delete(chat: Chat, bot_id: int) -> bool:
