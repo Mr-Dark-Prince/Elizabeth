92bcf12a41fa83243f9a3a45226c2b1b3b441ce2
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 16:53:48 +0530

    Update memes.py

--- ubotindo/modules/memes.py
+++ ubotindo/modules/memes.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import random
 import re
 import time
@@ -24,15 +8,15 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, run_async
 from telegram.utils.helpers import escape_markdown
 
-import ubotindo.modules.helper_funcs.fun_strings as fun
-from ubotindo import DEV_USERS, LOGGER, SUDO_USERS, SUPPORT_USERS, dispatcher
-from ubotindo.modules.disable import (
+import Elizabeth.modules.helper_funcs.fun_strings as fun
+from Elizabeth import DEV_USERS, LOGGER, SUDO_USERS, SUPPORT_USERS, dispatcher
+from Elizabeth.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from ubotindo.modules.helper_funcs.alternate import typing_action
-from ubotindo.modules.helper_funcs.extraction import extract_user
-from ubotindo.modules.helper_funcs.filters import CustomFilters
+from Elizabeth.modules.helper_funcs.alternate import typing_action
+from Elizabeth.modules.helper_funcs.extraction import extract_user
+from Elizabeth.modules.helper_funcs.filters import CustomFilters
 
 
 @run_async
