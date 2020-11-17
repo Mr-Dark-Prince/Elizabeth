ba5a8d8891003e1e6902d3fe6926be1a1cce4bd3
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 16:13:41 +0530

    Update antiflood.py

--- ubotindo/modules/antiflood.py
+++ ubotindo/modules/antiflood.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import html
 from typing import Optional
 
@@ -22,13 +6,13 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from ubotindo import dispatcher
-from ubotindo.modules.connection import connected
-from ubotindo.modules.helper_funcs.alternate import send_message, typing_action
-from ubotindo.modules.helper_funcs.chat_status import is_user_admin, user_admin
-from ubotindo.modules.helper_funcs.string_handling import extract_time
-from ubotindo.modules.log_channel import loggable
-from ubotindo.modules.sql import antiflood_sql as sql
+from Elizabeth import dispatcher
+from Elizabeth.modules.connection import connected
+from Elizabeth.modules.helper_funcs.alternate import send_message, typing_action
+from Elizabeth.modules.helper_funcs.chat_status import is_user_admin, user_admin
+from Elizabeth.modules.helper_funcs.string_handling import extract_time
+from Elizabeth.modules.log_channel import loggable
+from Elizabeth.modules.sql import antiflood_sql as sql
 
 FLOOD_GROUP = 3
 
