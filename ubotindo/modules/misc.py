7adda1e57cf845d85cbc9f1ef37ad840444aaa39
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 17:03:06 +0530

    Update misc.py

--- ubotindo/modules/misc.py
+++ ubotindo/modules/misc.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import datetime
 import codecs
 import html
@@ -43,7 +27,7 @@ from telegram.ext import CommandHandler, Filters, run_async
 from telegram.utils.helpers import escape_markdown, mention_html
 from tswift import Song
 
-from ubotindo import (
+from Elizabeth import (
     DEV_USERS,
     OWNER_ID,
     SUDO_USERS,
@@ -53,13 +37,13 @@ from ubotindo import (
     dispatcher,
     spamwtc,
 )
-from ubotindo.__main__ import GDPR, STATS, USER_INFO
-from ubotindo.modules.disable import DisableAbleCommandHandler
-from ubotindo.modules.global_bans import check_cas
-from ubotindo.modules.helper_funcs.alternate import send_action, typing_action
-from ubotindo.modules.helper_funcs.extraction import extract_user
-from ubotindo.modules.helper_funcs.filters import CustomFilters
-from ubotindo.modules.sql.afk_sql import is_afk
+from Elizabeth.__main__ import GDPR, STATS, USER_INFO
+from Elizabeth.modules.disable import DisableAbleCommandHandler
+from Elizabeth.modules.global_bans import check_cas
+from Elizabeth.modules.helper_funcs.alternate import send_action, typing_action
+from Elizabeth.modules.helper_funcs.extraction import extract_user
+from Elizabeth.modules.helper_funcs.filters import CustomFilters
+from Elizabeth.modules.sql.afk_sql import is_afk
 
 
 @run_async
