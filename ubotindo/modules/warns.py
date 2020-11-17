fa17f0fb1943d4f6c3bf13b61462c4174bc96b18
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 18:22:56 +0530

    Update warns.py

--- ubotindo/modules/warns.py
+++ ubotindo/modules/warns.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import html
 import re
 
@@ -37,27 +21,27 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from ubotindo import dispatcher  # BAN_STICKER
-from ubotindo.modules.connection import connected
-from ubotindo.modules.disable import DisableAbleCommandHandler
-from ubotindo.modules.helper_funcs.alternate import typing_action
-from ubotindo.modules.helper_funcs.chat_status import (
+from Elizabeth import dispatcher  # BAN_STICKER
+from Elizabeth.modules.connection import connected
+from Elizabeth.modules.disable import DisableAbleCommandHandler
+from Elizabeth.modules.helper_funcs.alternate import typing_action
+from Elizabeth.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from ubotindo.modules.helper_funcs.extraction import (
+from Elizabeth.modules.helper_funcs.extraction import (
     extract_text,
     extract_user,
     extract_user_and_text,
 )
-from ubotindo.modules.helper_funcs.filters import CustomFilters
-from ubotindo.modules.helper_funcs.misc import split_message
-from ubotindo.modules.helper_funcs.string_handling import split_quotes
-from ubotindo.modules.log_channel import loggable
-from ubotindo.modules.sql import warns_sql as sql
+from Elizabeth.modules.helper_funcs.filters import CustomFilters
+from Elizabeth.modules.helper_funcs.misc import split_message
+from Elizabeth.modules.helper_funcs.string_handling import split_quotes
+from Elizabeth.modules.log_channel import loggable
+from Elizabeth.modules.sql import warns_sql as sql
 
 WARN_HANDLER_GROUP = 9
 CURRENT_WARNING_FILTER_STRING = "<b>Current warning filters in this chat:</b>\n"
