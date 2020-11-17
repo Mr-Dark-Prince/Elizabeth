f1016b3afaa934554a5f3286dfe4a53eddbc6660
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 16:21:25 +0530

    Update blacklist.py

--- ubotindo/modules/blacklist.py
+++ ubotindo/modules/blacklist.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import html
 import re
 
@@ -22,17 +6,17 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler, run_async
 from telegram.utils.helpers import mention_html
 
-import ubotindo.modules.sql.blacklist_sql as sql
-from ubotindo import LOGGER, dispatcher
-from ubotindo.modules.connection import connected
-from ubotindo.modules.disable import DisableAbleCommandHandler
-from ubotindo.modules.helper_funcs.alternate import send_message, typing_action
-from ubotindo.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from ubotindo.modules.helper_funcs.extraction import extract_text
-from ubotindo.modules.helper_funcs.misc import split_message
-from ubotindo.modules.helper_funcs.string_handling import extract_time
-from ubotindo.modules.log_channel import loggable
-from ubotindo.modules.warns import warn
+import Elizabeth.modules.sql.blacklist_sql as sql
+from Elizabeth import LOGGER, dispatcher
+from Elizabeth.modules.connection import connected
+from Elizabeth.modules.disable import DisableAbleCommandHandler
+from Elizabeth.modules.helper_funcs.alternate import send_message, typing_action
+from Elizabeth.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Elizabeth.modules.helper_funcs.extraction import extract_text
+from Elizabeth.modules.helper_funcs.misc import split_message
+from Elizabeth.modules.helper_funcs.string_handling import extract_time
+from Elizabeth.modules.log_channel import loggable
+from Elizabeth.modules.warns import warn
 
 BLACKLIST_GROUP = 11
 
