ab3de3d4b7fd32070a66ea4a4acea8c91afd708a
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-17 17:10:24 +0530

    Update notes.py

--- ubotindo/modules/notes.py
+++ ubotindo/modules/notes.py
@@ -1,19 +1,3 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import ast
 import re
 from html import escape
@@ -30,15 +14,15 @@ from telegram.ext import CallbackQueryHandler, CommandHandler, Filters, MessageH
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import mention_html
 
-import ubotindo.modules.sql.notes_sql as sql
-from ubotindo import LOGGER, MESSAGE_DUMP, dispatcher
-from ubotindo.modules.connection import connected
-from ubotindo.modules.disable import DisableAbleCommandHandler
-from ubotindo.modules.helper_funcs.alternate import typing_action
-from ubotindo.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
-from ubotindo.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from ubotindo.modules.helper_funcs.msg_types import get_note_type
-from ubotindo.modules.helper_funcs.string_handling import (
+import Elizabeth.modules.sql.notes_sql as sql
+from Elizabeth import LOGGER, MESSAGE_DUMP, dispatcher
+from Elizabeth.modules.connection import connected
+from Elizabeth.modules.disable import DisableAbleCommandHandler
+from Elizabeth.modules.helper_funcs.alternate import typing_action
+from Elizabeth.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from Elizabeth.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from Elizabeth.modules.helper_funcs.msg_types import get_note_type
+from Elizabeth.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
