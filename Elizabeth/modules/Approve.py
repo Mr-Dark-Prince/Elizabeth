6c43a700bc6df0e0465a0f1f693b935ddddb392b
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-18 23:28:37 +0530

    Update Approve.py

--- Elizabeth/modules/Approve.py
+++ Elizabeth/modules/Approve.py
@@ -3,7 +3,6 @@ from Elizabeth.events import register
 from telethon import types
 from telethon.tl import functions
 
-client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
 db = client["Elizabeth"]
 approved_users = db.approve
