17298d077077af94ac11254cf93049d6adde8c95
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-20 03:52:45 +0530

    Update song.py

--- Elizabeth/modules/song.py
+++ Elizabeth/modules/song.py
@@ -1,15 +1,21 @@
 from telegram import ParseMode, Update, Bot
 from telegram.ext import run_async
+from Elizabeth.events import register
+from Elizabeth import client
 from Elizabeth.modules.disable import DisableAbleCommandHandler
 from Elizabeth import dispatcher
 from Elizabeth.modules.Elizabeth import dl, db
 import os
 
-@run_async
-def song(update, context):
+@register(pattern="^/song (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+
     message = update.effective_message
     text = message.text[len('/song '):]
     dl(cmd)
+    cmd = event.pattern_match.group(1)
     sng = db.get('title') + '.mp3'
     title = db.get('title')
     reply_text = f"Finding **{title}**"
