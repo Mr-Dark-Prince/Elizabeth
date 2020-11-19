cc96d4f83162756edbbe4d8d5d929c2e73d60e02
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-19 19:07:51 +0530

    Update songs.py

--- Elizabeth/modules/songs.py
+++ Elizabeth/modules/songs.py
@@ -7,8 +7,6 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from validators.url import url
 
 from Elizabeth.events import register
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import CMD_HELP, name_dl, reply_id, runcmd, song_dl, video_dl, yt_search
 
 # =========================================================== #
 #                           STRINGS                           #
