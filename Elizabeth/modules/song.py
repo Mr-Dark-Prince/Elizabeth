2c877a7484b0a7463aca0eafc09a7594e9838980
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-19 22:30:27 +0530

    Create song.py

---
+++ Elizabeth/modules/song.py
@@ -0,0 +1,8 @@
+from Elizabeth import client
+import glob
+import os
+import spotdl
+import subprocess
+from telethon import types
+from telethon.tl import functions
+from Elizabeth.events import register
