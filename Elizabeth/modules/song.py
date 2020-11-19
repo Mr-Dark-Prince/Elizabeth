453c05eff8c18b17462685c1e26b4954a606be26
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-19 22:55:36 +0530

    Update song.py

--- Elizabeth/modules/song.py
+++ Elizabeth/modules/song.py
@@ -8,35 +8,34 @@ from telethon.tl import functions
 from Elizabeth.events import register
 
 
+import instantmusic,subprocess
+os.system("rm -rf *.mp3")
+    
 
 @register(pattern="^/song (.*)")
 async def _(event):
     if event.fwd_from:
         return
-
     cmd = event.pattern_match.group(1)
-    cmnd = f"{cmd}"
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    try:
-        subprocess.run(["spotdl", "-s", cmnd, "-q", "best"])
-        subprocess.run(
-            'for f in *.opus; do      mv -- "$f" "${f%.mp3}.mp3"; done', shell=True
-        )
-        l = glob.glob("*.mp3")
-        loa = l[0]
-        await event.reply("sending the song")
-        await client.send_file(
-            event.chat_id,
-            loa,
-            force_document=False,
-            allow_cache=False,
-            supports_streaming=True,
-            caption=cmd,
-            reply_to=reply_to_id,
-        )
-        os.system("rm -rf *.mp3")
-    except Exception:
-        await event.reply("I am getting too many requests !\nPlease try again later.")
+    await event.reply("ok finding the song")
+    os.system("instantmusic -q -s "+cmd)
+    bro = 'for f in *.webm; do      mv -- "$f" "${f%.webm}.mp3"; done'
+    os.system(bro)
+    l = glob.glob("*.mp3")
+    loa = l[0]
+    await event.reply("sending the song")
+    await event.client.send_file(
+                event.chat_id,
+                loa,
+                force_document=True,
+                allow_cache=False,
+                caption=cmd,
+                reply_to=reply_to_id
+            )
+    os.system("rm -rf *.mp3")
+    subprocess.check_output("rm -rf *.mp3",shell=True)
+
 
