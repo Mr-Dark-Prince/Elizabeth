22ad7c60fb817a71c14c5e5c9bacb274bb246e0e
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-20 21:00:18 +0530

    Update __main__.py

--- Elizabeth/__main__.py
+++ Elizabeth/__main__.py
@@ -58,17 +58,7 @@ buttons += [[InlineKeyboardButton(text="🔒 Close the Menu 🔒",
 
 
 HELP_STRINGS = f"""
-Hello there! My name is *{dispatcher.bot.first_name}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
-the things I can help you with.
-
-*Main* commands available:
- × /start: Starts me, can be used to check i'm alive or no...
- × /help: PM's you this message.
- × /help <module name>: PM's you info about that module.
- × /settings: in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
- \nClick on the buttons below to get documentation about specific modules!"""
+Click on the buttons below to get documentation about specific modules!"""
 
 
 STAFF_HELP_STRINGS = """Hey there staff users. Nice to see you :)
