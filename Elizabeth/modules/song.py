9956189ea5dc17a70ce54cd66eedbe53c991c07b
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-20 10:54:52 +0530

    Update song.py

--- Elizabeth/modules/song.py
+++ Elizabeth/modules/song.py
@@ -37,8 +37,7 @@ except:
 	from youtubesearchpython import SearchVideos 
 	pass
 
-
-@borg.on(admin_cmd(pattern="song(?: |$)(.*)"))
+@register(pattern="^/song (.*)")
 async def download_video(v_url):  
 
     lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
