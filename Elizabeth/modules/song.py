2567f23f504f8a31bd800cadee4c7cb2b60202a6
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-20 22:42:29 +0530

    Update song.py

--- Elizabeth/modules/song.py
+++ Elizabeth/modules/song.py
@@ -129,4 +129,8 @@ async def download_video(v_url):
         \nby *{rip_data['uploader']}*")
         await v_url.client.send_file(
             v_url.chat_id,
-            f"{rip_data['id']}.mp3")
+            f"{rip_data['id']}.mp3"
+            supports_streaming=True,
+            parse_mode="html",
+            reply_to=reply_to_id,
+            )
