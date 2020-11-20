d0a7ce18ce31127b6f6bc90157fb8ffbd62c9952
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-20 22:59:08 +0530

    Update song.py

--- Elizabeth/modules/song.py
+++ Elizabeth/modules/song.py
@@ -39,8 +39,9 @@ except:
 	pass
 
 @register(pattern="^/song (.*)")
-async def download_video(v_url):  
-
+async def download_video(v_url):
+     
+    reply_to_id = await reply_id(v_url)
     lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
 
     if not sender.id == me.id:
