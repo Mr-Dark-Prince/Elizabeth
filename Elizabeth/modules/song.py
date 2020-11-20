ed22ade6cab133de30cad28d12a3f5863cf9153c
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-21 01:29:26 +0530

    Update song.py

--- Elizabeth/modules/song.py
+++ Elizabeth/modules/song.py
@@ -10,6 +10,19 @@ from Elizabeth import client
 from Elizabeth.events import register
 
 
+async def edit_or_reply(event, text, parse_mode=None, link_preview=None):
+    link_preview = link_preview or False
+    parse_mode = parse_mode or "md"
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to = await event.get_reply_message()
+        if reply_to:
+            return await reply_to.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        return await event.reply(text, parse_mode=parse_mode, link_preview=link_preview)
+    return await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+
+
 @register(pattern="^/song (.*)")
 async def _(event):
     reply_to_id = await reply_id(event)
