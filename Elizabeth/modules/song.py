93674895c00ed3551a10872c6707fe86937c5a6b
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-20 16:34:33 +0530

    Update song.py

--- Elizabeth/modules/song.py
+++ Elizabeth/modules/song.py
@@ -44,9 +44,6 @@ async def download_video(v_url):
     lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
     if lazy.reply_to_msg_id:
         reply_to_id = lazy.reply_to_msg_id
-    else:       
-    if not sender.id == me.id:
-        rkp = await lazy.reply("`processing...`")
     else:
     	rkp = await lazy.edit("`processing...`")   
     url = v_url.pattern_match.group(1)
