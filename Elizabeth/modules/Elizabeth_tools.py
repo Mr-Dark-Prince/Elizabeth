31dbd159671a8806ecf14d56b473688458d38e63
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-21 18:18:49 +0530

    Update Elizabeth_tools.py

--- Elizabeth/modules/Elizabeth_tools.py
+++ Elizabeth/modules/Elizabeth_tools.py
@@ -1,4 +1,6 @@
 __help__ = """
+ ➩ /getsticker: Reply to a sticker to me to upload its raw PNG file.
+ ➩ /kang: Reply to a sticker to add it to your pack.
  ➩ /weather <city>: Gets weather information of particular place!
  ➩ /zip: reply to a telegram file to compressing in .zip format
  ➩ /unzip: reply to a telegram file to decompress it from the .zip format
