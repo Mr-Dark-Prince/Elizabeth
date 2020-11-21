6c7e99d9a057f3b6a366901f4327ac2516aa1cfc
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-21 18:44:10 +0530

    Update userinfo.py

--- Elizabeth/modules/userinfo.py
+++ Elizabeth/modules/userinfo.py
@@ -163,8 +163,6 @@ All bios are displayed on /info command.
  × /bio: Will get your or another user's bio. This cannot be set by yourself.
  × /setme <text>: Will set your info
  × /me: Will get your or another user's info
-× /stickerid: Reply to a sticker to me to tell you its file ID.
-
 
 An example of setting a bio for yourself:
 `/setme I work for Telegram`; Bio is set to yourself.
