7fbe9a2a3d5c5575f0558e596d744b6600bbb243
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-21 17:17:42 +0530

    Update weather.py

--- Elizabeth/modules/weather.py
+++ Elizabeth/modules/weather.py
@@ -124,14 +124,6 @@ def weather(update, context):
             return
 
 
-__help__ = """
-Weather module:
-
- × /weather <city>: Gets weather information of particular place!
-
- \* To prevent spams weather command and the output will be deleted after 30 seconds
-"""
-
 __mod_name__ = "Weather"
 
 WEATHER_HANDLER = DisableAbleCommandHandler("weather", weather, pass_args=True)
