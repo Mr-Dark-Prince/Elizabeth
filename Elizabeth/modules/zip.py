5e92d392b81d60adf096e8be63b6afbd79a4578b
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-21 16:11:25 +0530

    Update zip.py

--- Elizabeth/modules/zip.py
+++ Elizabeth/modules/zip.py
@@ -70,3 +70,121 @@ def zipdir(path, ziph):
         for file in files:
             ziph.write(os.path.join(root, file))
             os.remove(os.path.join(root, file))
+
+from datetime import datetime
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from telethon.tl.types import DocumentAttributeVideo
+
+extracted = TEMP_DOWNLOAD_DIRECTORY + "extracted/"
+thumb_image_path = TEMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
+if not os.path.isdir(extracted):
+    os.makedirs(extracted)
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (
+                await client(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (
+            await client(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+
+@register(pattern="^/unzip")
+async def _(event):
+    if event.fwd_from:
+        return
+
+    mone = await event.reply("Processing ...")
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        reply_message = await event.get_reply_message()
+        try:
+            c_time = time.time()
+            downloaded_file_name = await tbot.download_media(
+                reply_message, TEMP_DOWNLOAD_DIRECTORY
+            )
+        except Exception as e:  
+            await mone.reply(str(e))
+        else:
+            end = datetime.now()
+            ms = (end - start).seconds
+
+        with zipfile.ZipFile(downloaded_file_name, "r") as zip_ref:
+            zip_ref.extractall(extracted)
+        filename = sorted(get_lst_of_files(extracted, []))
+        await event.reply("Unzipping now")
+        for single_file in filename:
+            if os.path.exists(single_file):
+                caption_rts = os.path.basename(single_file)
+                force_document = True
+                supports_streaming = False
+                document_attributes = []
+                if single_file.endswith((".mp4", ".mp3", ".flac", ".webm")):
+                    metadata = extractMetadata(createParser(single_file))
+                    duration = 0
+                    width = 0
+                    height = 0
+                    if metadata.has("duration"):
+                        duration = metadata.get("duration").seconds
+                    if os.path.exists(thumb_image_path):
+                        metadata = extractMetadata(createParser(thumb_image_path))
+                        if metadata.has("width"):
+                            width = metadata.get("width")
+                        if metadata.has("height"):
+                            height = metadata.get("height")
+                    document_attributes = [
+                        DocumentAttributeVideo(
+                            duration=duration,
+                            w=width,
+                            h=height,
+                            round_message=False,
+                            supports_streaming=True,
+                        )
+                    ]
+                try:
+                    await tbot.send_file(
+                        event.chat_id,
+                        single_file,
+                        force_document=force_document,
+                        supports_streaming=supports_streaming,
+                        allow_cache=False,
+                        reply_to=event.message.id,
+                        attributes=document_attributes
+                    )
+                except Exception as e:
+                    await tbot.send_message(
+                        event.chat_id,
+                        "{} caused `{}`".format(caption_rts, str(e)),
+                        reply_to=event.message.id,
+                    )
+                    continue
+                os.remove(single_file)
+        os.remove(downloaded_file_name)
+
+
+def get_lst_of_files(input_directory, output_lst):
+    filesinfolder = os.listdir(input_directory)
+    for file_name in filesinfolder:
+        current_file_name = os.path.join(input_directory, file_name)
+        if os.path.isdir(current_file_name):
+            return get_lst_of_files(current_file_name, output_lst)
+        output_lst.append(current_file_name)
+    return output_lst
+
