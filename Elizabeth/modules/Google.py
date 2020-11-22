bd3a7f1ca709f0f41725729f956f8c6a7c8b4a88
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-22 13:50:30 +0530

    Update Google.py

--- Elizabeth/modules/Google.py
+++ Elizabeth/modules/Google.py
@@ -12,6 +12,26 @@ from re import findall
 import html2text
 
 
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await client(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
+
+
 @register(pattern="^/google (.*)") 
 async def _(event):
     if event.fwd_from:
