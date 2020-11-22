597b4101fa6abb3486d1a6e6275b4aec9018e827
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-22 20:20:18 +0530

    Update backups.py

--- Elizabeth/modules/backups.py
+++ Elizabeth/modules/backups.py
@@ -371,7 +371,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups"
+__mod_name__ = "BACKUPS"
 
 __help__ = """
 *Only for chat administrator:*
