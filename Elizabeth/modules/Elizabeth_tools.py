2a1faac0f7bc3649e68ae4f96e1ec9d821342953
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-22 18:39:26 +0530

    Update Elizabeth_tools.py

--- Elizabeth/modules/Elizabeth_tools.py
+++ Elizabeth/modules/Elizabeth_tools.py
@@ -1,6 +1,7 @@
 __help__ = """
  ➩ /imdb <movie name> Get imdb result from imbd.com
  ➩ /google <text input> Gets google search result
+ ➩ /img <object> Gets google image results
  ➩ /song <song name> Get that song from YouTube 
  ➩ /weather <city>: Gets weather information of particular place!
  ➩ /zip: reply to a telegram file to compressing in .zip format
