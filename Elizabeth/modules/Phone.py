b6a9ca68f52b87cbfc901f94526f8cc320d2e561
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-23 01:20:20 +0530

    Update Phone.py

--- Elizabeth/modules/Phone.py
+++ Elizabeth/modules/Phone.py
@@ -8,6 +8,25 @@ import os
 from Elizabeth import client
 from Elizabeth.events import register
 
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await client(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
+
 @register(pattern=r'^/phone (.*)')
 async def phone(event): 
     if event.is_group:
