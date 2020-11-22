7d305d3dc2586212d579f695e22fd8a1a8c11015
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-23 00:47:14 +0530

    Update Playstore.py

--- Elizabeth/modules/Playstore.py
+++ Elizabeth/modules/Playstore.py
@@ -6,6 +6,26 @@ from Elizabeth import client
 from Elizabeth.events import register
 
 
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await client(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
+
+
 @register(pattern="^/app (.*)")
 async def apk(e):
     if e.is_group:
