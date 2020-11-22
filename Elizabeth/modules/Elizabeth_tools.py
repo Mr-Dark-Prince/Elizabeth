59032bd03ee8cdae23dd78c778b1fa11665cc2f7
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-22 15:49:37 +0530

    Update Elizabeth_tools.py

--- Elizabeth/modules/Elizabeth_tools.py
+++ Elizabeth/modules/Elizabeth_tools.py
@@ -1,5 +1,6 @@
 __help__ = """
- ➩ /song <Song name> Get that song from YouTube 
+ ➩ /google <text input> Gets google search result
+ ➩ /song <song name> Get that song from YouTube 
  ➩ /weather <city>: Gets weather information of particular place!
  ➩ /zip: reply to a telegram file to compressing in .zip format
  ➩ /unzip: reply to a telegram file to decompress it from the .zip format
