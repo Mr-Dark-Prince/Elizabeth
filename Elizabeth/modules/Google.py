124bd56f3eadc6245315935e7b4e2a9b4900226a
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-22 18:12:09 +0530

    Update Google.py

--- Elizabeth/modules/Google.py
+++ Elizabeth/modules/Google.py
@@ -41,7 +41,7 @@ async def _(event):
         return
     if event.is_group:
      if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-       await event.reply("")
+       await event.reply("😜 Hai.. You are not admin..🤭 You can't use this command.. But you can use in my pm🙈")
        return
     # SHOW_DESCRIPTION = False
     input_str = event.pattern_match.group(1) # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
