736d96273a9c30a1e20fd9d41b987a5d2d7c25e4
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-22 18:13:07 +0530

    Update Google_image.py

--- Elizabeth/modules/Google_image.py
+++ Elizabeth/modules/Google_image.py
@@ -38,7 +38,7 @@ async def img_sampler(event):
         return
      if event.is_group:
        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-          await event.reply("Are you admin 🙄?")
+          await event.reply("😜 Hai.. You are not admin..🤭 You can't use this command.. But you can use in my pm🙈")
           return
      query = event.pattern_match.group(1)
      jit = f'"{query}"'
