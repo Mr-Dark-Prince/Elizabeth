6faa439abbaffe6f4067b00b6e49dc01a517b8ea
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-22 21:59:58 +0530

    Update cust_filters.py

--- Elizabeth/modules/cust_filters.py
+++ Elizabeth/modules/cust_filters.py
@@ -545,24 +545,24 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- × /filters: List all active filters saved in the chat.
+ ➩ /filters: List all active filters saved in the chat.
 
 *Admin only:*
- × /filter <keyword> <reply message>: Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ ➩ /filter <keyword> <reply message>: Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
- × /stop <filter keyword>: Stop that filter.
+ ➩ /stop <filter keyword>: Stop that filter.
 
 *Chat creator only:*
- × /rmallfilter: Stop all chat filters at once.
+ ➩ /rmallfilter: Stop all chat filters at once.
 
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "FILTERS"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
