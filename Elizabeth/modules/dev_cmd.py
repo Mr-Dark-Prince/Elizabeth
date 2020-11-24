76aaea587d76c414ecfcbc8042d84bb1ec92c702
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-24 19:11:55 +0530

    Create dev_cmd.py

---
+++ Elizabeth/modules/dev_cmd.py
@@ -0,0 +1,32 @@
+__help__ = """
+Here is all the staff's commands. Users above has the command access for all commands below.
+
+*OWNER*
+× /broadcast: Send a broadcast message to all chat that i'm currently in.
+× /staffids: Get all staff's you have.
+× /ip: Sends the bot's IP address to ssh in if necessary (PM only).
+
+*DEV USERS*
+× /gitpull: Pull latest update.
+× /reboot: Restart the bot.
+× /dbcleanup: Clean my invalid database.
+× /leavemutedchats: Leave all chats where i can't send message.
+× /leave <chatid>: Tell me to leave the given group. (alias /leavechat /leavegroup).
+× /stats: List of all blacklists, filters, federations, gbans, etc from all group.
+× /getlink <chatid>: Get chat invite link.
+× /sysinfo: Get my system info.
+
+*SUDO USERS*
+× /snipe <chatid> <string>: Tell me to send a message to the given chat.
+× /echo <string>: Like snipe but on the current chat.
+× /chatlist: Get the list of chat that i'm currently in.
+× /ping: Start a ping test.
+× /speedtest: Start a speedtest from my server.
+
+*SUPPORT USERS*
+× /gban <userid>: global ban a user.
+× /ungban <userid>: remove currently gbanned user.
+× /gbanlist: Get the list of currently gbanned users.
+"""
+
+__mod_name__ = "🌳 DEV"
