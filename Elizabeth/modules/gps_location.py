cb0e2b77149bccc7430ad8c4b3b6a4fd6360d141
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-26 14:39:28 +0530

    Create gps_location.py

---
+++ Elizabeth/modules/gps_location.py
@@ -0,0 +1,7 @@
+import os
+from Elizabeth import client
+from geopy.geocoders import Nominatim
+from Elizabeth.events import register
+from Elizabeth import *
+from telethon import *
+from telethon.tl import *
