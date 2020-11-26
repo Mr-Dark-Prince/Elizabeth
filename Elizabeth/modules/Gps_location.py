678f0c29e0d5d241f4668c37a0de4cb438536cbe
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-26 14:44:33 +0530

    Update Gps_location.py

--- Elizabeth/modules/Gps_location.py
+++ Elizabeth/modules/Gps_location.py
@@ -5,3 +5,26 @@ from Elizabeth.events import register
 from Elizabeth import *
 from telethon import *
 from telethon.tl import *
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await
+             Client(functions.channels.GetParticipantRequest(chat,
+                                                           user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id)
+                         )).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+GMAPS_LOC = "https://maps.googleapis.com/maps/api/geocode/json"
