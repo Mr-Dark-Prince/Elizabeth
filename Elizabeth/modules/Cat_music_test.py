e0427273f5cb05e6f91af8a02a87ea0c42488e27
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-27 11:14:22 +0530

    Update Cat_music_test.py

--- Elizabeth/modules/Cat_music_test.py
+++ Elizabeth/modules/Cat_music_test.py
@@ -1,1 +1,10 @@
-#
+import asyncio
+import base64
+import os
+from pathlib import Path
+from Elizabeth import client
+from Elizabeth.events import register
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from validators.url import url
