4c074816bd244efc54b0dbc78d7712be1965d16d
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-27 11:31:29 +0530

    Update Cat_music_test.py

--- Elizabeth/modules/Cat_music_test.py
+++ Elizabeth/modules/Cat_music_test.py
@@ -74,3 +74,125 @@ async def edit_or_reply(
     await event.client.send_file(event.chat_id, file_name, caption=caption)
     await event.delete()
     os.remove(file_name)
+
+
+async def yt_search(cat):
+    try:
+        cat = urllib.parse.quote(cat)
+        html = urllib.request.urlopen(
+            "https://www.youtube.com/results?search_query=" + cat
+        )
+        user_data = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+        video_link = None
+        if user_data:
+            video_link = "https://www.youtube.com/watch?v=" + user_data[0]
+        if video_link:
+            return video_link
+        return "Couldnt fetch results"
+    except:
+        return "Couldnt fetch results"
+
+
+async def extract_time(cat, time_val):
+    if any(time_val.endswith(unit) for unit in ("m", "h", "d", "w")):
+        unit = time_val[-1]
+        time_num = time_val[:-1]  # type: str
+        if not time_num.isdigit():
+            cat.edit("Invalid time amount specified.")
+            return ""
+        if unit == "m":
+            bantime = int(time.time() + int(time_num) * 60)
+        elif unit == "h":
+            bantime = int(time.time() + int(time_num) * 60 * 60)
+        elif unit == "d":
+            bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
+        elif unit == "w":
+            bantime = int(time.time() + int(time_num) * 7 * 24 * 60 * 60)
+        else:
+            # how even...?
+            return ""
+        return bantime
+    await cat.edit(
+        f"Invalid time type specified. Expected m , h , d or w but got: {time_val[-1]}"
+    )
+    return ""
+
+
+song_dl = "youtube-dl --force-ipv4 --write-thumbnail -o './temp/%(title)s.%(ext)s' --extract-audio --audio-format mp3 --audio-quality {QUALITY} {video_link}"
+thumb_dl = "youtube-dl --force-ipv4 -o './temp/%(title)s.%(ext)s' --write-thumbnail --skip-download {video_link}"
+video_dl = "youtube-dl --force-ipv4 --write-thumbnail  -o './temp/%(title)s.%(ext)s' -f '[filesize<20M]' {video_link}"
+name_dl = (
+    "youtube-dl --force-ipv4 --get-filename -o './temp/%(title)s.%(ext)s' {video_link}"
+)
+
+
+
+@register(pattern="^/song (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(2):
+        query = event.pattern_match.group(2)
+    elif reply:
+        if reply.message:
+            query = reply.message
+    else:
+        await edit_or_reply(event, "`What I am Supposed to find `")
+        return
+    cat = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+    catevent = await edit_or_reply(event, "`wi8..! I am finding your song....`")
+    video_link = await yt_search(str(query))
+    if not url(video_link):
+        return await catevent.edit(
+            f"Sorry!. I can't find any related video/audio for `{query}`"
+        )
+    cmd = event.pattern_match.group(1)
+    if cmd == "song":
+        q = "128k"
+    song_cmd = song_dl.format(QUALITY=q, video_link=video_link)
+    # thumb_cmd = thumb_dl.format(video_link=video_link)
+    name_cmd = name_dl.format(video_link=video_link)
+    try:
+        cat = Get(cat)
+        await event.client(cat)
+    except BaseException:
+        pass
+    stderr = (await runcmd(song_cmd))[1]
+    if stderr:
+        return await catevent.edit(f"**Error :** `{stderr}`")
+    catname, stderr = (await runcmd(name_cmd))[:2]
+    if stderr:
+        return await catevent.edit(f"**Error :** `{stderr}`")
+    # stderr = (await runcmd(thumb_cmd))[1]
+    catname = os.path.splitext(catname)[0]
+    # if stderr:
+    #    return await catevent.edit(f"**Error :** `{stderr}`")
+    song_file = Path(f"{catname}.mp3")
+    if not os.path.exists(song_file):
+        return await catevent.edit(
+            f"Sorry!. I can't find any related video/audio for `{query}`"
+        )
+    await catevent.edit("`yeah..! i found something wi8..🥰`")
+    catthumb = Path(f"{catname}.jpg")
+    if not os.path.exists(catthumb):
+        catthumb = Path(f"{catname}.webp")
+    elif not os.path.exists(catthumb):
+        catthumb = None
+
+    await event.client.send_file(
+        event.chat_id,
+        song_file,
+        force_document=False,
+        caption=f"<b><i>➥ Song :- {query}</i></b>\n<b><i>➥ Uploaded by :- {hmention}</i></b>",
+        thumb=catthumb,
+        supports_streaming=True,
+        parse_mode="html",
+        reply_to=reply_to_id,
+    )
+    await catevent.delete()
+    for files in (catthumb, song_file):
+        if files and os.path.exists(files):
+            os.remove(files)
+
