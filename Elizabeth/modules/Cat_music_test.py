10c6a05476b4a88680831ae5fa5b5972606a361a
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-27 12:13:32 +0530

    Update Cat_music_test.py

--- Elizabeth/modules/Cat_music_test.py
+++ Elizabeth/modules/Cat_music_test.py
@@ -13,8 +13,6 @@ from validators.url import url
 
 async def reply_id(event):
     reply_to_id = None
-    if event.sender_id in Config.SUDO_USERS:
-        reply_to_id = event.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     return reply_to_id
