361239641bc0a948901607fb68370d6b37e07104
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-29 11:09:27 +0530

    Add files via upload

---
+++ Elizabeth/modules/Elizabeth_tools.py
@@ -0,0 +1,21 @@
+__help__ = """
+ ➩ /imdb <movie name> Get imdb result from imbd.com
+
+ ➩ /app <app name> Search application from play store
+
+ ➩ /phone <full number> Check Details 
+
+ ➩ /song <songname artist(optional)>: uploads the song in it's best quality available
+
+ ➩ /video <songname artist(optional)>: uploads the video song in it's best quality available
+
+ ➩ /weather <city>: Gets weather information of particular place!
+
+ ➩ /datetime <country code> Get date and time by provided country 
+
+ ➩ /zip: Reply to a telegram file to compressing in .zip format
+
+ ➩ /unzip: Reply to a telegram file to decompress it from the .zip format
+"""
+
+__mod_name__ = "📇 TOOLS"
---
+++ Elizabeth/modules/dev_cmd.py
@@ -0,0 +1,28 @@
+__help__ = """
+Here is all the staff's commands.
+
+*OWNER*
+× /broadcast: Send a broadcast message to all chat that i'm currently in.
+× /staffids: Get all staff's you have.
+× /ip: Sends the bot's IP address to ssh in if necessary (PM only).
+
+*DEV USERS*
+× /dbcleanup: Clean my invalid database.
+× /leavemutedchats: Leave all chats where i can't send message.
+× /leave <chatid>: Tell me to leave the given group. (alias /leavechat /leavegroup).
+× /stats: List of all blacklists, filters, federations, gbans, etc from all group.
+× /getlink <chatid>: Get chat invite link.
+
+*SUDO USERS*
+× /snipe <chatid> <string>: Tell me to send a message to the given chat.
+× /echo <string>: Like snipe but on the current chat.
+× /chatlist: Get the list of chat that i'm currently in.
+× /ping: Start a ping test.
+
+*SUPPORT USERS*
+× /gban <userid>: global ban a user.
+× /ungban <userid>: remove currently gbanned user.
+× /gbanlist: Get the list of currently gbanned users.
+"""
+
+__mod_name__ = "🌳 DEV"
