808511b8e8b4943c80450858c5bf832cc368a32b
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-30 01:32:55 +0530

    Update antiflood.py

--- Elizabeth/modules/antiflood.py
+++ Elizabeth/modules/antiflood.py
@@ -389,7 +389,7 @@ will result in restricting that user.
  1w = 1 week
  """
 
-__mod_name__ = "CONTROL"
+__mod_name__ = "FLOOD"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
