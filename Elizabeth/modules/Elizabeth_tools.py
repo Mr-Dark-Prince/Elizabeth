67666dd53f2e3b9b5edbf3b8dab592378d3adeb6
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-29 20:42:32 +0530

    Update Elizabeth_tools.py

--- Elizabeth/modules/Elizabeth_tools.py
+++ Elizabeth/modules/Elizabeth_tools.py
@@ -5,10 +5,6 @@ __help__ = """
 
  ➩ /phone <full number> Check Details 
 
- ➩ /song <songname artist(optional)>: uploads the song in it's best quality available
-
- ➩ /video <songname artist(optional)>: uploads the video song in it's best quality available
-
  ➩ /weather <city>: Gets weather information of particular place!
 
  ➩ /datetime <country code> Get date and time by provided country 
