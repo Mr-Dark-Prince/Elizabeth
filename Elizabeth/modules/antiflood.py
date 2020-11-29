4a216022affe31d09cd9f214d67c76724272b7c1
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-30 01:32:33 +0530

    Update antiflood.py

--- Elizabeth/modules/antiflood.py
+++ Elizabeth/modules/antiflood.py
@@ -365,18 +365,6 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-*BLACKLIST*
-Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
-
-*NOTE*: Blacklists do not affect group admins.
-
- ➩ /blacklist: View the current blacklisted words.
-
-Admin only:
- ➩ /addblacklist <triggers>: Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- ➩ /unblacklist <triggers>: Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- ➩ /rmblacklist <triggers>: Same as above.
- ➩ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>: Action to perform when someone sends blacklisted words.
 
 *FLOOD CONTROL*
 You know how sometimes, people join, send 100 messages, and ruin your chat? With antiflood, that happens no more!
@@ -399,8 +387,6 @@ will result in restricting that user.
  6h = 6 hours
  3d = 3 days
  1w = 1 week
-
- ⚠️ `Read from top`
  """
 
 __mod_name__ = "CONTROL"
