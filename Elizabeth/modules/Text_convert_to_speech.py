8795bca9c927b896db9ce458259323f4184304e4
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-30 14:18:30 +0530

    Update Text_convert_to_speech.py

--- Elizabeth/modules/Text_convert_to_speech.py
+++ Elizabeth/modules/Text_convert_to_speech.py
@@ -1,1 +1,35 @@
-#
+
+from Elizabeth import client
+import os
+
+from gtts import gTTS
+from gtts import gTTSError
+from telethon import *
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.tl.types import *
+
+from Elizabeth import *
+
+from Elizabeth.events import register
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await
+             client(functions.channels.GetParticipantRequest(chat,
+                                                           user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id)
+                         )).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
