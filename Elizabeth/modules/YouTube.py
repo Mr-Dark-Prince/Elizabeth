699916d9d28e54f23a0d180e4d0a74613f28a1e9
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-30 16:34:45 +0530

    Update YouTube.py

--- Elizabeth/modules/YouTube.py
+++ Elizabeth/modules/YouTube.py
@@ -1,1 +1,17 @@
-#
+from Elizabeth import client
+import os
+import time
+from telethon import types
+from telethon.tl import functions
+from telethon.tl.types import DocumentAttributeAudio
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import ContentTooShortError
+from youtube_dl.utils import DownloadError
+from youtube_dl.utils import ExtractorError
+from youtube_dl.utils import GeoRestrictedError
+from youtube_dl.utils import MaxDownloadsReached
+from youtube_dl.utils import PostProcessingError
+from youtube_dl.utils import UnavailableVideoError
+from youtube_dl.utils import XAttrMetadataError
+from Elizabeth.events import register
+
