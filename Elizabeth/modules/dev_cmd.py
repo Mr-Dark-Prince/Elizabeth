4e4125ce1a9d0b76f36714b61a05459eee293094
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-30 21:33:51 +0530

    Update dev_cmd.py

--- Elizabeth/modules/dev_cmd.py
+++ Elizabeth/modules/dev_cmd.py
@@ -8,22 +8,26 @@ Here is all the staff's commands.
 ➩ /ip: Sends the bot's IP address to ssh in if necessary (PM only).
 
 *DEV USERS*
+➩ /gitpull: Pull latest update.
+➩ /reboot: Restart the bot.
 ➩ /dbcleanup: Clean my invalid database.
 ➩ /leavemutedchats: Leave all chats where i can't send message.
 ➩ /leave <chatid>: Tell me to leave the given group. (alias /leavechat /leavegroup).
 ➩ /stats: List of all blacklists, filters, federations, gbans, etc from all group.
 ➩ /getlink <chatid>: Get chat invite link.
+➩ /sysinfo: Get my system info.
 
 *SUDO USERS*
 ➩ /snipe <chatid> <string>: Tell me to send a message to the given chat.
 ➩ /echo <string>: Like snipe but on the current chat.
 ➩ /chatlist: Get the list of chat that i'm currently in.
 ➩ /ping: Start a ping test.
+➩ /speedtest: Start a speedtest from my server.
 
 *SUPPORT USERS*
 ➩ /gban <userid>: global ban a user.
 ➩ /ungban <userid>: remove currently gbanned user.
-➩ /gbanlist: Get the list of currently gbanned users.
+➩ /gbanlist: Get the list of currently gbanned users."""
 """
 
 __mod_name__ = "DEVS"
