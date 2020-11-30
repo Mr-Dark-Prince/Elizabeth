a46819d51ad00ebb8872f27c1d5972433d76813c
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-11-30 19:43:05 +0530

    Create dev_extra.py

---
+++ Elizabeth/modules/dev_extra.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+import sys
+from time import sleep
+
+from Elizabeth import dispatcher
+from Elizabeth import DEV_USERS, dispatcher
+from Elizabeth.modules.helper_funcs.filters import CustomFilters
+
+from telegram import TelegramError, Update
+from telegram.ext import CallbackContext, CommandHandler, run_async
+
+
+@run_async
+def leave(update, context):
+    bot = context.bot
+    args = context.args
+    if args:
+        chat_id = str(args[0])
+        try:
+            bot.leave_chat(int(chat_id))
+            update.effective_message.reply_text("Beep boop, I left that soup!.")
+        except TelegramError:
+            update.effective_message.reply_text(
+                "Beep boop, I could not leave that group(dunno why tho).")
+    else:
+        update.effective_message.reply_text("Send a valid chat ID")
+
+
