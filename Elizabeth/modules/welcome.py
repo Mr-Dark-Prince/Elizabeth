4e1efc96070d9bad815d4b4ece0430dbc77685d8
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-12-05 02:48:06 +0530

    Update welcome.py

--- Elizabeth/modules/welcome.py
+++ Elizabeth/modules/welcome.py
@@ -213,7 +213,7 @@ def new_member(update, context):
 
                 context.bot.send_message(
                     MESSAGE_DUMP,
-                    "UserbotindoBot have been added to <pre>{}</pre> with ID: \n<pre>{}</pre>".format(
+                    "Elizabeth have been added to <pre>{}</pre> with ID: \n<pre>{}</pre>".format(
                         chat.title,
                         chat.id),
                     parse_mode=ParseMode.HTML,
