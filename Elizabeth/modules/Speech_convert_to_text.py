e84aab5e913cd972aa429e96b2d441d268060a41
MHD SALIM <73339924+P-RINC-E@users.noreply.github.com>
2020-12-05 22:38:14 +0530

    Added language codes for🍼 amul babies 😁

--- Elizabeth/modules/Speech_convert_to_text.py
+++ Elizabeth/modules/Speech_convert_to_text.py
@@ -105,6 +105,15 @@ __help__ = """
  ➩ /tts <lang | text>: Returns a speech note of the text provided..
 
  ➩ /stt: Reply to a voice message(⚠️ English only) to extract text from it...
+
+*Language Codes*
+
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
 
 
