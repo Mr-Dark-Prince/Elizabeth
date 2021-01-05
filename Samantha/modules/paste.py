634f4e90b2dbc10f5040e0b91f5135510288fabd
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 00:02:03 +0530

    Update paste.py

--- Samantha/modules/paste.py
+++ Samantha/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Samantha import dispatcher
+from Samantha.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
