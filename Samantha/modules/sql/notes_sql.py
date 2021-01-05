65276449d9cd7c8d13e3d2d48701bd12d0d35f3c
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 01:00:11 +0530

    Update notes_sql.py

--- Samantha/modules/sql/notes_sql.py
+++ Samantha/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from SaitamaRobot.modules.helper_funcs.msg_types import Types
-from SaitamaRobot.modules.sql import BASE, SESSION
+from Samantha.modules.helper_funcs.msg_types import Types
+from Samantha.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
