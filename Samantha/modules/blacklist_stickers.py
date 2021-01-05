9ad3a0a88993a131f0dc1bdbb7a3bc5e7a0b660a
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-05 23:28:09 +0530

    Update blacklist_stickers.py

--- Samantha/modules/blacklist_stickers.py
+++ Samantha/modules/blacklist_stickers.py
@@ -1,17 +1,17 @@
 import html
 from typing import Optional
 
-import SaitamaRobot.modules.sql.blsticker_sql as sql
-from SaitamaRobot import LOGGER, dispatcher
-from SaitamaRobot.modules.connection import connected
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.alternate import send_message
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from SaitamaRobot.modules.helper_funcs.misc import split_message
-from SaitamaRobot.modules.helper_funcs.string_handling import extract_time
+import Samantha.modules.sql.blsticker_sql as sql
+from Samantha import LOGGER, dispatcher
+from Samantha.modules.connection import connected
+from Samantha.modules.disable import DisableAbleCommandHandler
+from Samantha.modules.helper_funcs.alternate import send_message
+from Samantha.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Samantha.modules.helper_funcs.misc import split_message
+from Samantha.modules.helper_funcs.string_handling import extract_time
 
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.warns import warn
+from Samantha.modules.log_channel import loggable
+from Samantha.modules.warns import warn
 from telegram import Chat, Message, ParseMode, Update, User, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
