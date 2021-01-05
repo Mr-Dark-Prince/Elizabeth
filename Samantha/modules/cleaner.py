f3cb379733ccd6bcc6ffb634f0c13fb1c2f1d727
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-05 23:31:55 +0530

    Update cleaner.py

--- Samantha/modules/cleaner.py
+++ Samantha/modules/cleaner.py
@@ -1,14 +1,14 @@
 import html
 
-from SaitamaRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from Samantha import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from Samantha.modules.disable import DisableAbleCommandHandler
+from Samantha.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from SaitamaRobot.modules.sql import cleaner_sql as sql
+from Samantha.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
