97762d898c1bb408393d741f03b54ad640174c1f
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 01:00:39 +0530

    Update reporting_sql.py

--- Samantha/modules/sql/reporting_sql.py
+++ Samantha/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from Samantha.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
