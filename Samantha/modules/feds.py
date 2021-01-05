e6b11dd0b764f2c7be6fc77eac7ddeac2cfac3cb
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-05 23:43:53 +0530

    Update feds.py

--- Samantha/modules/feds.py
+++ Samantha/modules/feds.py
@@ -6,8 +6,8 @@ import time
 import uuid
 from io import BytesIO
 
-import SaitamaRobot.modules.sql.feds_sql as sql
-from SaitamaRobot import (
+import Samantha.modules.sql.feds_sql as sql
+from Samantha import (
     EVENT_LOGS,
     LOGGER,
     SUPPORT_CHAT,
@@ -17,15 +17,15 @@ from SaitamaRobot import (
     WOLVES,
     dispatcher,
 )
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.alternate import send_message
-from SaitamaRobot.modules.helper_funcs.chat_status import is_user_admin
-from SaitamaRobot.modules.helper_funcs.extraction import (
+from Samantha.modules.disable import DisableAbleCommandHandler
+from Samantha.modules.helper_funcs.alternate import send_message
+from Samantha.modules.helper_funcs.chat_status import is_user_admin
+from Samantha.modules.helper_funcs.extraction import (
     extract_unt_fedban,
     extract_user,
     extract_user_fban,
 )
-from SaitamaRobot.modules.helper_funcs.string_handling import markdown_parser
+from Samantha.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
