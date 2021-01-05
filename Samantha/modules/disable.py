2466bf943bc6aae06e749bdf7ff658ab491280c4
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-05 23:40:00 +0530

    Update disable.py

--- Samantha/modules/disable.py
+++ Samantha/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from SaitamaRobot.modules.helper_funcs.misc import is_module_loaded
+from Samantha import dispatcher
+from Samantha.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from Samantha.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -20,12 +20,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from SaitamaRobot.modules.helper_funcs.chat_status import (
+    from Samantha.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from SaitamaRobot.modules.sql import disable_sql as sql
+    from Samantha.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
