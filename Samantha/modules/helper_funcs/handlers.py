37e7c34d570476fc8dbfd83a9b21295288273a1f
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 01:19:08 +0530

    Update handlers.py

--- Samantha/modules/helper_funcs/handlers.py
+++ Samantha/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import SaitamaRobot.modules.sql.blacklistusers_sql as sql
-from SaitamaRobot import ALLOW_EXCL
-from SaitamaRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import Samantha.modules.sql.blacklistusers_sql as sql
+from Samantha import ALLOW_EXCL
+from Samantha import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
