bf1dc494c356445dda589d25e544c4c0cbe0f0b6
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-05 23:47:26 +0530

    Update gettime.py

--- Samantha/modules/gettime.py
+++ Samantha/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from SaitamaRobot import TIME_API_KEY, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Samantha import TIME_API_KEY, dispatcher
+from Samantha.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
