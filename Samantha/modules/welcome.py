322826fa3d8a4d88f022ec342c130995a8b41a11
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 00:30:34 +0530

    Update welcome.py

--- Samantha/modules/welcome.py
+++ Samantha/modules/welcome.py
@@ -4,8 +4,8 @@ import re
 import time
 from functools import partial
 
-import SaitamaRobot.modules.sql.welcome_sql as sql
-from SaitamaRobot import (
+import Samantha.modules.sql.welcome_sql as sql
+from Samantha import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -17,18 +17,18 @@ from SaitamaRobot import (
     dispatcher,
     JOIN_LOGGER,
 )
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from Samantha.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from SaitamaRobot.modules.helper_funcs.msg_types import get_welcome_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+from Samantha.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from Samantha.modules.helper_funcs.msg_types import get_welcome_type
+from Samantha.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.sql.global_bans_sql import is_user_gbanned
+from Samantha.modules.log_channel import loggable
+from Samantha.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
