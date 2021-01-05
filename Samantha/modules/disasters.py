0810c83b679b349c2de7fe98fe4cd9cc6620d65c
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-05 23:40:56 +0530

    Update disasters.py

--- Samantha/modules/disasters.py
+++ Samantha/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from SaitamaRobot import (
+from Samantha import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,13 +13,13 @@ from SaitamaRobot import (
     WOLVES,
     dispatcher,
 )
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from Samantha.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot.modules.log_channel import gloggable
+from Samantha.modules.helper_funcs.extraction import extract_user
+from Samantha.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
