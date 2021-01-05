7a5c871ce73a37b46d2d1c810d2032bc3bbc190e
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 00:25:54 +0530

    Update wallpaper.py

--- Samantha/modules/wallpaper.py
+++ Samantha/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from SaitamaRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from Samantha import SUPPORT_CHAT, WALL_API, dispatcher
+from Samantha.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
