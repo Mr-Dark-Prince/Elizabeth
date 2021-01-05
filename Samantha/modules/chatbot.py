f111236b449c06d8ea66497e8dc67f2ab58fe49e
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-05 23:30:54 +0530

    Update chatbot.py

--- Samantha/modules/chatbot.py
+++ Samantha/modules/chatbot.py
@@ -3,14 +3,14 @@ import html
 # AI module using Intellivoid's Coffeehouse API by @TheRealPhoenix
 from time import sleep, time
 
-import SaitamaRobot.modules.sql.chatbot_sql as sql
+import Samantha.modules.sql.chatbot_sql as sql
 from coffeehouse.api import API
 from coffeehouse.exception import CoffeeHouseError as CFError
 from coffeehouse.lydia import LydiaAI
-from SaitamaRobot import AI_API_KEY, OWNER_ID, SUPPORT_CHAT, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
-from SaitamaRobot.modules.log_channel import gloggable
+from Samantha import AI_API_KEY, OWNER_ID, SUPPORT_CHAT, dispatcher
+from Samantha.modules.helper_funcs.chat_status import user_admin
+from Samantha.modules.helper_funcs.filters import CustomFilters
+from Samantha.modules.log_channel import gloggable
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import (
