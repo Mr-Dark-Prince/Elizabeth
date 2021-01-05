f540801664a80e2fa6371aeb6c70df42d7780ce6
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 00:50:32 +0530

    Update approve_sql.py

--- Samantha/modules/sql/approve_sql.py
+++ Samantha/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from SaitamaRobot.modules.sql import BASE, SESSION
+from Samantha.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
