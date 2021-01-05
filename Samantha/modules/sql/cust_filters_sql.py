75af45648a683e5d99169c3a1233409e11e714f5
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 00:55:45 +0530

    Update cust_filters_sql.py

--- Samantha/modules/sql/cust_filters_sql.py
+++ Samantha/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from SaitamaRobot.modules.helper_funcs.msg_types import Types
-from SaitamaRobot.modules.sql import BASE, SESSION
+from Samantha.modules.helper_funcs.msg_types import Types
+from Samantha.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
