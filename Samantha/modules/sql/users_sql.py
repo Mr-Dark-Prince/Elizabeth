da037bf0b763aaa474ab5540c80e09db7bd4ed0d
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 01:03:13 +0530

    Update users_sql.py

--- Samantha/modules/sql/users_sql.py
+++ Samantha/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.sql import BASE, SESSION
+from Samantha import dispatcher
+from Samantha.modules.sql import BASE, SESSION
 from sqlalchemy import (
     Column,
     ForeignKey,
