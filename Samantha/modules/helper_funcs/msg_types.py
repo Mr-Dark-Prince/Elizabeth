c51213da110b72b154818e67bcf172795b2fca51
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 01:20:33 +0530

    Update msg_types.py

--- Samantha/modules/helper_funcs/msg_types.py
+++ Samantha/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from SaitamaRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from Samantha.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
