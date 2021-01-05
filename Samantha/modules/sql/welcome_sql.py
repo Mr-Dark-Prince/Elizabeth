4943fd9348af80849d593aa4a1cc6c736beb369d
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 01:05:16 +0530

    Update welcome_sql.py

--- Samantha/modules/sql/welcome_sql.py
+++ Samantha/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from SaitamaRobot.modules.helper_funcs.msg_types import Types
-from SaitamaRobot.modules.sql import BASE, SESSION
+from Samantha.modules.helper_funcs.msg_types import Types
+from Samantha.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
