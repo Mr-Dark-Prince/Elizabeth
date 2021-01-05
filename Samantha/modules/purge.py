331265fb5e131017c2eb1248ef02cb091b920a5d
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 00:08:22 +0530

    Update purge.py

--- Samantha/modules/purge.py
+++ Samantha/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from SaitamaRobot import telethn
-from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
+from Samantha import telethn
+from Samantha.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
