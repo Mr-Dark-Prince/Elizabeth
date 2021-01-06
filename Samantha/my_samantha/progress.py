f76938987312e940f9f83e158acb8a9e9742414a
MHD SALIM <73339924+A-DARK-PRINCE@users.noreply.github.com>
2021-01-06 10:11:52 +0530

    Update progress.py

--- Samantha/my_samantha/progress.py
+++ Samantha/my_samantha/progress.py
@@ -6,30 +6,35 @@ from .tools import humanbytes, time_formatter
 
 
 async def progress(
-    current, total, event, start, type_of_ps, file_name=None, is_cancelled=None
+    current, total, gdrive, start, prog_type, file_name=None, is_cancelled=False
 ):
-    """Generic progress_callback for uploads and downloads."""
     now = time.time()
     diff = now - start
     if is_cancelled is True:
         raise CancelProcess
+
     if round(diff % 10.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "[{0}{1}] {2}%\n".format(
-            "".join(["▰" for i in range(math.floor(percentage / 10))]),
-            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+        elapsed_time = round(diff)
+        eta = round((total - current) / speed)
+        if "upload" in prog_type.lower():
+            status = "Uploading"
+        elif "download" in prog_type.lower():
+            status = "Downloading"
+        else:
+            status = "Unknown"
+        progress_str = "`{0}` | [{1}{2}] `{3}%`".format(
+            status,
+            "".join("●" for i in range(math.floor(percentage / 10))),
+            "".join("○" for i in range(10 - math.floor(percentage / 10))),
             round(percentage, 2),
         )
-        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
-            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        tmp = (
+            f"{progress_str}\n"
+            f"`{humanbytes(current)} of {humanbytes(total)}"
+            f" @ {humanbytes(speed)}`\n"
+            f"`ETA` -> {time_formatter(eta)}\n"
+            f"`Duration` -> {time_formatter(elapsed_time)}"
         )
-        if file_name:
-            await event.edit(
-                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
-            )
-        else:
-            await event.edit("{}\n{}".format(type_of_ps, tmp))
+        await gdrive.edit(f"`{prog_type}`\n\n" f"`Status`\n{tmp}")
