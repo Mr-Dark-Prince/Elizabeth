de9e8677662f7ab9025f11da02138419303b6c19
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:23:41 +0530

    Update __init__.py

--- Harry_Potter/modules/sql/__init__.py
+++ Harry_Potter/modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from priscia import DB_URI
+from Harry_Potter import DB_URI
 
 
 def start() -> scoped_session:
