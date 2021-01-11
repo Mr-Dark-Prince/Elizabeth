5e27cd5d395c865a6cf7e9d101d807f96e99a9a1
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:34:38 +0530

    Update log_channel_sql.py

--- Harry_Potter/modules/sql/log_channel_sql.py
+++ Harry_Potter/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from priscia.modules.sql import BASE, SESSION
+from Harry_Potter.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
