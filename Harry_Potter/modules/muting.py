615e2a54b9148c93f903fc5ec40d861065ffa6b3
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 23:26:33 +0530

    Update muting.py

--- Harry_Potter/modules/muting.py
+++ Harry_Potter/modules/muting.py
@@ -6,17 +6,17 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters
 from telegram.utils.helpers import mention_html
 
-from priscia import LOGGER, dispatcher
-from priscia.modules.helper_funcs.admin_rights import user_can_ban
-from priscia.modules.helper_funcs.chat_status import (
+from Harry_Potter import LOGGER, dispatcher
+from Harry_Potter.modules.helper_funcs.admin_rights import user_can_ban
+from Harry_Potter.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     is_user_admin,
     user_admin,
 )
-from priscia.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from priscia.modules.helper_funcs.string_handling import extract_time
-from priscia.modules.log_channel import loggable
+from Harry_Potter.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from Harry_Potter.modules.helper_funcs.string_handling import extract_time
+from Harry_Potter.modules.log_channel import loggable
 
 
 @bot_admin
