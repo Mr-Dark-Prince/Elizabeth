334cc7fce39dc5c89ca9edf8c3b002f6fc64065a
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:33:56 +0530

    Update locks_sql.py

--- Harry_Potter/modules/sql/locks_sql.py
+++ Harry_Potter/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from priscia.modules.sql import SESSION, BASE
+from Harry_Potter.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
