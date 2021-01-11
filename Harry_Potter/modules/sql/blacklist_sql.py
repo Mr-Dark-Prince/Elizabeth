f5c2315d995b3dd2c21a0eef2b59b291bf72a58b
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:26:30 +0530

    Update blacklist_sql.py

--- Harry_Potter/modules/sql/blacklist_sql.py
+++ Harry_Potter/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from priscia.modules.sql import SESSION, BASE
+from Harry_Potter.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
