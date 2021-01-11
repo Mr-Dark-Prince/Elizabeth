ec11c767b3a02a70cd1d67c55400bbe80550b859
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:36:57 +0530

    Update rules_sql.py

--- Harry_Potter/modules/sql/rules_sql.py
+++ Harry_Potter/modules/sql/rules_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from priscia.modules.sql import SESSION, BASE
+from Harry_Potter.modules.sql import SESSION, BASE
 
 
 class Rules(BASE):
