99d4fd7e826e62950ba1fab2bfd9ca2c8bf61106
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:39:48 +0530

    Update warns_sql.py

--- Harry_Potter/modules/sql/warns_sql.py
+++ Harry_Potter/modules/sql/warns_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
 
-from priscia.modules.sql import SESSION, BASE
+from Harry_Potter.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
