7f673aff979e5f62eb3808f6fc59ba449e513503
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:33:20 +0530

    Update last_fm_sql.py

--- Harry_Potter/modules/sql/last_fm_sql.py
+++ Harry_Potter/modules/sql/last_fm_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from priscia.modules.sql import BASE, SESSION
+from Harry_Potter.modules.sql import BASE, SESSION
 
 
 class LastFMUsers(BASE):
