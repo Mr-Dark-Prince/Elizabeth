71aa0b56c886fab638da419144f49bd6cf672da4
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:27:02 +0530

    Update connection_sql.py

--- Harry_Potter/modules/sql/connection_sql.py
+++ Harry_Potter/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from priscia.modules.sql import SESSION, BASE
+from Harry_Potter.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
