33a7c6c74475138c14367f39701c911c575fd4c7
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:24:14 +0530

    Update admin_sql.py

--- Harry_Potter/modules/sql/admin_sql.py
+++ Harry_Potter/modules/sql/admin_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, String
 
-from priscia.modules.sql import SESSION, BASE
+from Harry_Potter.modules.sql import SESSION, BASE
 
 
 class PermanentPin(BASE):
