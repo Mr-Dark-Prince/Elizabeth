de4b0d7e8c139f5fb18aea40d039e8c65b3c66c0
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:12:12 +0530

    Update extraction.py

--- Harry_Potter/modules/helper_funcs/extraction.py
+++ Harry_Potter/modules/helper_funcs/extraction.py
@@ -3,8 +3,8 @@ from typing import List, Optional
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
-from priscia import LOGGER
-from priscia.modules.users import get_user_id
+from Harry_Potter import LOGGER
+from Harry_Potter.modules.users import get_user_id
 
 
 def id_from_reply(message):
