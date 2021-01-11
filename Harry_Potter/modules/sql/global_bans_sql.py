376653ced23ecbc71cc8363021ccefcec3dc106b
MHD SALIM <73339924+D-A-R-K-P-R-I-N-C-E@users.noreply.github.com>
2021-01-11 21:32:50 +0530

    Update global_bans_sql.py

--- Harry_Potter/modules/sql/global_bans_sql.py
+++ Harry_Potter/modules/sql/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from priscia.modules.sql import BASE, SESSION
+from Harry_Potter.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
