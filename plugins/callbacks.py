dac98be970be49229b1577e579f70ebaa87281cb
Mr Dark Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-06-13 16:45:59 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,8 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
-                    InlineKeyboardButton("About Me", callback_data="about_data")
-                ],
-                [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("🖨️Source", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("⚠️About", callback_data="about_data")
                 ]
             ]
         )
@@ -69,12 +65,8 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
-                ],
-                [
-                    InlineKeyboardButton("BACK", callback_data="help_data"),
-                    InlineKeyboardButton("CLOSE", callback_data="close_data"),
+                    InlineKeyboardButton("↩️BACK", callback_data="help_data"),
+                    InlineKeyboardButton("❌️CLOSE", callback_data="close_data"),
                 ]                
             ]
         )
